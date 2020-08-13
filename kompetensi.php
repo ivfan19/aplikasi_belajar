5771d93cb892c88070c7d4cc62464595dee23fe3
ivfan19 <ivfan19@gmail.com>
2020-08-13 07:32:43 +0700

    first commit

---
+++ kompetensi.php
@@ -0,0 +1,22 @@
+<?php
+
+include_once 'template/header.html'
+
+?>
+  <main role="main" class="col-md-9 ml-sm-auto col-lg-10 px-md-4 mt-3">
+  
+
+    <h2>Kompetensi</h2>
+
+    
+      <?php
+      $path = '\nindy\doc\KOMPETENSI.pdf';
+
+      echo "<iframe src='".$path."' width=\"100%\" style=\"height:400%\"></iframe>";
+
+      ?>
+
+  </main>
+<?php 
+include_once 'template/footer.html'
+?>
---
+++ soal_latihan.php
@@ -0,0 +1,301 @@
+<?php
+
+include_once 'template/header.html'
+
+?>
+  <main role="main" class="col-md-9 ml-sm-auto col-lg-10 px-md-4 mt-3">
+  
+
+  <div class="row d-flex justify-content-between mb-3">
+    <h2>Materi</h2>
+    <a href="#jawaban" class="btn btn-md btn-primary">Kebawah</a>
+  </div>
+    
+      <?php
+      $path = '\nindy\doc\soal emodul.pdf';
+
+      echo "<iframe src='".$path."' width=\"100%\" style=\"height:400%\"></iframe>";
+
+      ?>
+
+    <h2 class="mt-5">Jawaban</h2>
+
+    <h5 class="my-5">I. Pilihan ganda</h5>
+    <div id="jawaban">
+      
+      <div class="row ml-2 mr-2">
+        <h6 class="col">1.</h6>
+      </div>
+
+      <div class="row mx-4">
+        <div class="col">
+          <input type="radio" id="1" name="1" value="a">
+          <label for="a">A</label><br>
+        </div>
+
+        <div class="col">
+          <input type="radio" id="1" name="1" value="b">
+          <label for="b">B</label><br>
+        </div>
+
+        <div class="col">
+          <input type="radio" id="1" name="1" value="c">
+          <label for="c">C</label>
+        </div>
+
+
+        <div class="col">
+          <input type="radio" id="1" name="1" value="d">
+          <label for="d">D</label>
+        </div>
+      </div>
+
+      <div class="row ml-2 mr-2">
+        <h6 class="col">2.</h6>
+      </div>
+
+      <div class="row mx-4">
+        <div class="col">
+          <input type="radio" id="2" name="2" value="a">
+          <label for="a">A</label><br>
+        </div>
+
+        <div class="col">
+          <input type="radio" id="2" name="2" value="b">
+          <label for="b">B</label><br>
+        </div>
+
+        <div class="col">
+          <input type="radio" id="2" name="2" value="c">
+          <label for="c">C</label>
+        </div>
+
+
+        <div class="col">
+          <input type="radio" id="2" name="2" value="d">
+          <label for="d">D</label>
+        </div>
+      </div>
+
+      <div class="row ml-2 mr-2">
+        <h6 class="col">3.</h6>
+      </div>
+
+      <div class="row mx-4">
+        <div class="col">
+          <input type="radio" id="3" name="3" value="a">
+          <label for="a">A</label><br>
+        </div>
+
+        <div class="col">
+          <input type="radio" id="3" name="3" value="b">
+          <label for="b">B</label><br>
+        </div>
+
+        <div class="col">
+          <input type="radio" id="3" name="3" value="c">
+          <label for="c">C</label>
+        </div>
+
+
+        <div class="col">
+          <input type="radio" id="3" name="3" value="d">
+          <label for="d">D</label>
+        </div>
+      </div>
+
+      <div class="row ml-2 mr-2">
+        <h6 class="col">4.</h6>
+      </div>
+
+      <div class="row mx-4">
+        <div class="col">
+          <input type="radio" id="4" name="4" value="a">
+          <label for="a">A</label><br>
+        </div>
+
+        <div class="col">
+          <input type="radio" id="4" name="4" value="b">
+          <label for="b">B</label><br>
+        </div>
+
+        <div class="col">
+          <input type="radio" id="4" name="4" value="c">
+          <label for="c">C</label>
+        </div>
+
+
+        <div class="col">
+          <input type="radio" id="4" name="4" value="d">
+          <label for="d">D</label>
+        </div>
+      </div>
+
+      <div class="row ml-2 mr-2">
+        <h6 class="col">5.</h6>
+      </div>
+
+      <div class="row mx-4">
+        <div class="col">
+          <input type="radio" id="5" name="5" value="a">
+          <label for="a">A</label><br>
+        </div>
+
+        <div class="col">
+          <input type="radio" id="5" name="5" value="b">
+          <label for="b">B</label><br>
+        </div>
+
+        <div class="col">
+          <input type="radio" id="5" name="5" value="c">
+          <label for="c">C</label>
+        </div>
+
+
+        <div class="col">
+          <input type="radio" id="5" name="5" value="d">
+          <label for="d">D</label>
+        </div>
+      </div>
+
+      <div class="row ml-2 mr-2">
+        <h6 class="col">6.</h6>
+      </div>
+
+      <div class="row mx-4">
+        <div class="col">
+          <input type="radio" id="6" name="6" value="a">
+          <label for="a">A</label><br>
+        </div>
+
+        <div class="col">
+          <input type="radio" id="6" name="6" value="b">
+          <label for="b">B</label><br>
+        </div>
+
+        <div class="col">
+          <input type="radio" id="6" name="6" value="c">
+          <label for="c">C</label>
+        </div>
+
+
+        <div class="col">
+          <input type="radio" id="6" name="6" value="d">
+          <label for="d">D</label>
+        </div>
+      </div>
+
+      <div class="row ml-2 mr-2">
+        <h6 class="col">7.</h6>
+      </div>
+
+      <div class="row mx-4">
+        <div class="col">
+          <input type="radio" id="7" name="7" value="a">
+          <label for="a">A</label><br>
+        </div>
+
+        <div class="col">
+          <input type="radio" id="7" name="7" value="b">
+          <label for="b">B</label><br>
+        </div>
+
+        <div class="col">
+          <input type="radio" id="7" name="7" value="c">
+          <label for="c">C</label>
+        </div>
+
+
+        <div class="col">
+          <input type="radio" id="7" name="7" value="d">
+          <label for="d">D</label>
+        </div>
+      </div>
+
+      <div class="row ml-2 mr-2">
+        <h6 class="col">8.</h6>
+      </div>
+
+      <div class="row mx-4">
+        <div class="col">
+          <input type="radio" id="8" name="8" value="a">
+          <label for="a">A</label><br>
+        </div>
+
+        <div class="col">
+          <input type="radio" id="8" name="8" value="b">
+          <label for="b">B</label><br>
+        </div>
+
+        <div class="col">
+          <input type="radio" id="8" name="1" value="c">
+          <label for="c">C</label>
+        </div>
+
+
+        <div class="col">
+          <input type="radio" id="8" name="8" value="d">
+          <label for="d">D</label>
+        </div>
+      </div>
+
+      <div class="row ml-2 mr-2">
+        <h6 class="col">9.</h6>
+      </div>
+
+      <div class="row mx-4">
+        <div class="col">
+          <input type="radio" id="9" name="9" value="a">
+          <label for="a">A</label><br>
+        </div>
+
+        <div class="col">
+          <input type="radio" id="9" name="9" value="b">
+          <label for="b">B</label><br>
+        </div>
+
+        <div class="col">
+          <input type="radio" id="9" name="9" value="c">
+          <label for="c">C</label>
+        </div>
+
+
+        <div class="col">
+          <input type="radio" id="9" name="9" value="d">
+          <label for="d">D</label>
+        </div>
+      </div>
+
+      <div class="row ml-2 mr-2">
+        <h6 class="col">10.</h6>
+      </div>
+
+      <div class="row mx-4">
+        <div class="col">
+          <input type="radio" id="10" name="10" value="a">
+          <label for="a">A</label><br>
+        </div>
+
+        <div class="col">
+          <input type="radio" id="10" name="10" value="b">
+          <label for="b">B</label><br>
+        </div>
+
+        <div class="col">
+          <input type="radio" id="10" name="10" value="c">
+          <label for="c">C</label>
+        </div>
+
+
+        <div class="col">
+          <input type="radio" id="10" name="10" value="d">
+          <label for="d">D</label>
+        </div>
+      </div>
+
+    </div>
+
+  </main>
+<?php 
+include_once 'template/footer.html'
+?>
